bc00d035c0cb258bdc6e6ebbd0e214e4ae42c8af
addyxvii <addisonbennett@gmail.com>
2020-11-02 10:26:07 -0700

    adding poloniex socket code

--- index.ts
+++ index.ts
@@ -1,10 +1,26 @@
 import express from 'express';
+import { WebsocketClient } from "poloniex-node-api";
+
+require('dotenv').config()
 // import socket from 'socket.io';
 
 const bittrex = require('node-bittrex-api'); 
 const app = express();
 const PORT = 8000;
+const key = process.env.POLONIEX_API_KEY;
+const secret = process.env.POLONIEX_API_SECRET;
+const channels = [1002, "BTC_ETH"];
+const websocket = new WebsocketClient({ key, secret, channels });
+
+console.log(key, secret);
 app.get('/', (req,res) => res.send('Express + TypeScript Server'));
+
+websocket.connect();  
+websocket.subscribe('BTC_ETH');
+websocket.on("open", () => console.log("open"));
+websocket.on("close", () => console.log("close"));
+websocket.on("error", (error) => console.error(error));
+websocket.on("message", (message) => console.info(message));
 
 bittrex.getorderbook( { market : 'BTC-ETH', depth : 10, type : 'both' }, function( data: any ) {
 
