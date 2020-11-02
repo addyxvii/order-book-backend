3a8205fcb9c33568961fd18432d937aa5ba82c01
addyxvii <addisonbennett@gmail.com>
2020-11-02 12:49:24 -0700

    adding poloniex socket code

--- api/v1/controllers/bittrex.ts
+++ api/v1/controllers/bittrex.ts
@@ -1,12 +1,11 @@
-import signalR from '@microsoft/signalr'; 
+const bittrex = require('node-bittrex-api'); 
 
-const hub = ['c3']
-const client = new signalR.client('wss://socket-v3.bittrex.com/signalr', hub);
-    client.serviceHandlers.messageReceived = messageReceived;
-    client.serviceHandlers.connected = () => {
-      console.log('Connected');
-      return resolve(client)
-    }
+bittrex.getorderbook( { market : 'BTC-ETH', depth : 10, type : 'both' }, function( data: any ) {
+
+    data.result.buy.forEach(function(dataset: any) { console.log(dataset); });
+    data.result.sell.forEach(function(dataset: any) { console.log(dataset); });
+});
+
 
 
 
--- index.ts
+++ index.ts
@@ -9,18 +9,15 @@ const app = express();
 const PORT = 8000;
 const key = process.env.POLONIEX_API_KEY;
 const secret = process.env.POLONIEX_API_SECRET;
-const channels = [1002, "BTC_ETH"];
-const websocket = new WebsocketClient({ key, secret, channels });
+const channels = ["BTC_ETH"];
+const poloniex = new WebsocketClient({ key, secret, channels });
 
-console.log(key, secret);
 app.get('/', (req,res) => res.send('Express + TypeScript Server'));
 
-websocket.connect();  
-websocket.subscribe('BTC_ETH');
-websocket.on("open", () => console.log("open"));
-websocket.on("close", () => console.log("close"));
-websocket.on("error", (error) => console.error(error));
-websocket.on("message", (message) => console.info(message));
+poloniex.connect();  
+poloniex.on("open", () => poloniex.subscribe('BTC_ETH'));
+poloniex.on("message", (message) => console.log(message));
+
 
 bittrex.getorderbook( { market : 'BTC-ETH', depth : 10, type : 'both' }, function( data: any ) {
 
